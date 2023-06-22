7f261a0a66d7f948d3fde255fd846169930aed05
bmeeder22 <bmeeder22@gmail.com>
2023-06-22 13:38:33 -0500

    Remving commented out code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 
 global.Buffer = global.Buffer || require('buffer').Buffer;
-// global.assert = global.assert || require('assert').assert;
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
